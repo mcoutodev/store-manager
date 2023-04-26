4512a59115f4748c91ce7308c5c9a24220d63e02
Marlon Couto <marloncouto199@gmail.com>
2023-04-26 20:06:31 -0300

    refactor: atualiza arquivos

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -2,20 +2,17 @@ const { productService } = require('../services');
 const errorMap = require('../utils/errorMap');
 
 const listProducts = async (_req, res) => {
-  const { type, message } = await productService.findAll();
-
-  if (type) return res.status(errorMap.mapError(type)).send({ message });
-
-  res.status(200).send(message);
+  const { message } = await productService.findAll();
+  res.status(200).json(message);
 };
 
 const getProduct = async (req, res) => {
   const { id } = req.params;
   const { type, message } = await productService.findById(id);
 
-  if (type) return res.status(errorMap.mapError(type)).send({ message });
+  if (type) return res.status(errorMap.mapError(type)).json({ message });
 
-  res.status(200).send(message);
+  res.status(200).json(message);
 };
 
 module.exports = { listProducts, getProduct };
