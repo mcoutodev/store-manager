969fb8459b5587f24cc9beb13bd2cb5e27679918
Marlon Couto <marloncouto199@gmail.com>
2023-04-28 14:48:11 -0300

    style: atualiza arquivos

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -9,17 +9,13 @@ const listProducts = async (_req, res) => {
 const getProduct = async (req, res) => {
   const { id } = req.params;
   const { type, message } = await productService.findById(id);
-
   if (type) return res.status(errorMap.mapError(type)).json({ message });
-
   res.status(200).json(message);
 };
 
 const createProduct = async (req, res) => {
   const { type, message } = await productService.createProduct(req.body);
-
   if (type) return res.status(errorMap.mapError(type)).json({ message });
-
   res.status(201).json(message);
 };
 
--- src/controllers/sale.controller.js
+++ src/controllers/sale.controller.js
@@ -3,18 +3,14 @@ const errorMap = require('../utils/errorMap');
 
 const createSale = async (req, res) => {
   const { type, message } = await saleService.createSale(req.body);
-
   if (type) return res.status(errorMap.mapError(type)).json({ message });
-
   res.status(201).json(message);
 };
 
 const getSale = async (req, res) => {
   const { id } = req.params;
   const { type, message } = await saleService.findById(id);
-
   if (type) return res.status(errorMap.mapError(type)).json({ message });
-
   res.status(200).json(message);
 };
 
